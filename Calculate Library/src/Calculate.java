cb0d5934f500efa3e4a9e2e8ac40f9bce1032652
clementng17 <17ngc04@millshighsch.org>
2016-08-30 09:26:35 -0700

    Calculate

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,23 @@
+
+public class Calculate {
+	public static double square(double number){
+		return(number*number);
+
+}
+	public static double cube(double number){
+		return(number*number*number);
+}
+	
+	public static double average(double number){
+		return();
+		answer		
+}
+	public static double toDegrees(double number){
+		return(number*360/3.14159);
+}
+	
+	public static double toRadians(double number){
+		return(number*3.14159);
+	}
+	
+	public static double 
