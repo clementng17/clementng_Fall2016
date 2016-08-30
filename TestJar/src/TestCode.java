2f826810490a04127e96300581f9ee9ab5ce45e3
clementng17 <17ngc04@millshighsch.org>
2016-08-30 08:28:21 -0700

    TestCode for Jar File demo

---
+++ TestJar/src/TestCode.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Clement Ng
+ * Date: August 30, 2016
+ *TestCode program -- used to show how to create jar file that I can turn into Classroom
+ */
+public class TestCode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
