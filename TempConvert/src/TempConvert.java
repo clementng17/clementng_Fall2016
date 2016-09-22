6150036c5af410e30aba956074e3d09660fcbb26
clementng17 <17ngc04@millshighsch.org>
2016-09-22 16:16:24 -0700

    temp convert 2

--- TempConvert/src/TempConvert.java
+++ TempConvert/src/TempConvert.java
@@ -3,14 +3,14 @@
  */
 
 /**
- * @author APCS1
+ * @author APCS1 Clement Ng
  *
  */
 public class TempConvert {
-	public static void main(String[]args) {
-		double F=32;
-		double C;
+	public static void main(String[] args) {
+		double F=32.0;
+		double C; 
 		C=(5/9)*(F-32);
-			System.out.println(“convert a temperature in fahrenheit to celsius”);
+		System.out.println(F + "in fahrenheit converts to" + " " + C + "in celsius");
 	}
 }
