f5b1bb729134e9b5735019afdfff40b782395848
clementng17 <17ngc04@millshighsch.org>
2017-03-09 08:51:35 -0800

    cell 8

--- TextExcel/src/textExcel/EmptyCell.java
+++ TextExcel/src/textExcel/EmptyCell.java
@@ -5,13 +5,16 @@ public class EmptyCell implements Cell {
 	@Override
 	public String abbreviatedCellText() {
 		// TODO Auto-generated method stub
-		return null;
+		return "          ";
 	}
 
 	@Override
 	public String fullCellText() {
 		// TODO Auto-generated method stub
-		return null;
+		return "\"\"";
+	}
+	public EmptyCell(){
+	}
 	}
 
 }
